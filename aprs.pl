c836e97c14d96990af27bea7568e5e9771c05ff1
Walter Francis <wally@theblackmoor.net>
2016-07-10 20:58:43 -0400

    Add day/month/year fields so we can set up future objects, such as hamfests, test sessions, etc
    Version check our program vs module code to ensure our fields are going to match

--- aprs.pl
+++ aprs.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-our $VERSION = '1.0';
+our $VERSION = '1.1';
 
 # APRS Schedule generator for Direwolf
 # Based on http://www.aprs.org/info/netsked.txt
@@ -13,11 +13,31 @@ our $VERSION = '1.0';
 # that template and this output and use that as the final configuration.  Repeat this every N minutes,
 # checking to see if the configuration has changed and if so restart direwolf so it picks up the new info.
 
+# Todo
+## Send packets directly, not using direwolf
+### beacon -d "APZ101 WIDE2-1" -t 60 0 '!DDMM.mmN/DDDMM.mmW$Comment'
+### aprx maybe?
+### Or directly in Perl using KISS
+### http://search.cpan.org/~rbdavison/Device-TNC-0.03/lib/Device/TNC/KISS.pm
+### http://search.cpan.org/dist/Ham-APRS-FAP/IS.pm
+
+## Improve object date options
+### Perhaps read a public iCal
+
 # This perl program is generic, so we use our module for station-specific settings
 use aprsobjects;
 
 # Get default settings from our aprsobjects module
-my ( $startinterval, $delayinterval, @outputs ) = get_defaults();
+my ( $moduleversion, $startinterval, $delayinterval, @outputs )
+    = get_defaults();
+
+# Test to make sure our versions match so we know our fields are going to be correct
+if ( $VERSION ne $moduleversion ) {
+    die 'Our program version number '
+        . $VERSION
+        . ' does not match our module version '
+        . $moduleversion . "\n";
+}
 
 # Get our objects from our aprsobjects module
 my (@objects) = get_objects();
@@ -27,18 +47,21 @@ my $delay = $startinterval;
 
 # Iterate through each entry in the @objects array
 foreach my $entry (@objects) {
-    my ($DAY,    $ENABLED, $STARTTIME, $ENDTIME, $TIMEBEFORE, $OBJNAME,
-        $MHZ,    $LAT,     $LON,       $FREQ,    $OFFSET,     $TONE,
-        $HEIGHT, $POWER,   $SYMBOL,    $COMMENT
+    my ($DOW,       $ENABLED, $MONTH,      $DAY,     $YEAR,
+        $STARTTIME, $ENDTIME, $TIMEBEFORE, $OBJNAME, $MHZ,
+        $LAT,       $LON,     $FREQ,       $OFFSET,  $TONE,
+        $HEIGHT,    $POWER,   $SYMBOL,     $COMMENT
     ) = split( /\|/xsm, $entry );
 
-    # Determine the time at which we start advertising the object before the official start time
+# Determine the time at which we start advertising the object before the official start time
     my $start;
 
     # Only modify the start time if we're a timed object
     if ( $STARTTIME ne 0 && $ENDTIME ne 0 ) {
-        # Set our time before to a negative value so we subtract time from our official start time
+
+# Set our time before to a negative value so we subtract time from our official start time
         my $timebefore = $TIMEBEFORE * -1;
+
         # Update the start value to the new time
         $start = update_delay( $STARTTIME, ${timebefore} );
     }
@@ -55,10 +78,28 @@ foreach my $entry (@objects) {
     my ( $sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst )
         = localtime();
     my $time = sprintf( "%02d%02d", $hour, $min );
+    $year += 1900;
+    $mon  += 1;
 
-    # Determine if this object matches the day or is an every-day object
-    if ( $ENABLED && ( $DAY == $wday || $DAY == -1 ) ) {
-        # Determine if the object should be advertised based on comparing our adjusted start time or always if start/end are 0
+# Determine if this object matches the day, is an every-day object, or is dated with todays date
+# If the object is enabled make sure that dated events are only matched on their proper date regardless
+# of DOW settings, so make sure daily vs single day vs dated events are tested separately.
+    if ($ENABLED
+
+        # This object matches a single-day event, not a dated event
+        && ((   ( $DOW eq $wday && $DAY !~ /\d+/ )
+
+                # This object matches a daily event, not a dated event
+                || ( $DOW eq -1 && $DAY !~ /\d+/ )
+            )
+
+# If it's not a single or daily event, test to see if this object matches todays date
+            || ( $MONTH eq $mon && $DAY eq $mday && $YEAR eq $year )
+        )
+        )
+    {
+
+# Determine if the object should be advertised based on comparing our adjusted start time or always if start/end are 0
         if (   ( $STARTTIME eq 0 && $ENDTIME eq 0 )
             || ( ( $start <= $time ) && ( $time <= $end ) ) )
         {
@@ -88,11 +129,29 @@ foreach my $entry (@objects) {
                 $comment_string = ' COMMENT="' . $COMMENT . '"';
             }
 
-            #Print the beacon info
-            print "\n# "
-                . $OBJNAME . " ("
-                . $STARTTIME . " - "
-                . $ENDTIME . ")\n";
+            # Print the beacon info
+            # Create a reasonable date string
+            my $datestring = '';
+
+            # If we have a start/end time, and we're not a dated event
+            if ( $STARTTIME && $DAY !~ /\d+/ ) {
+                $datestring = ' (' . $STARTTIME . ' -' . $ENDTIME . ')';
+            }
+
+            # If we're a dated event
+            if ( $DAY =~ /\d+/ ) {
+                $datestring
+                    = ' ('
+                    . $MONTH . '/'
+                    . $DAY . '/'
+                    . $YEAR . ') ('
+                    . $STARTTIME . ' - '
+                    . $ENDTIME . ') ';
+
+            }
+
+            print "\n# " . $OBJNAME . $datestring . "\n";
+
             foreach my $output (@outputs) {
                 print 'OBEACON '
                     . $output
@@ -115,7 +174,7 @@ foreach my $entry (@objects) {
                     . $power_string
                     . $comment_string . "\n";
 
-                # Update the delay value so each object advertises $delayinterval seconds after the previous one
+# Update the delay value so each object advertises $delayinterval seconds after the previous one
                 $delay = update_delay( $delay, $delayinterval );
             }
         }
--- aprsobjects.pm
+++ aprsobjects.pm
@@ -7,6 +7,9 @@ require Exporter;
 # Set default options
 sub get_defaults {
 
+    # Version control our module to make sure our field expections are correct
+    my $moduleversion = '1.1';
+
     # How long to delay our initial beacon advertisement, in direwolf format (min:sec)
     my $startinterval = '0:30';
 
@@ -16,7 +19,7 @@ sub get_defaults {
     # Define our output paths for every object
     my @outputs = ( 'sendto=IG', 'via="WIDE1-1,WIDE2-1"' );
 
-    return ( $startinterval, $delayinterval, @outputs );
+    return ( $moduleversion, $startinterval, $delayinterval, @outputs );
 }
 
 # Objects array is a pipe (|) separated list of fields
@@ -24,11 +27,14 @@ sub get_defaults {
 ## General Formatting is whatever Direwolf requires for the given field, see examples, Direwolf docs, etc
 ## Suggestion:  All day or every day events at the end so emphemeral events are advertised sooner
 
-# DAY - Sun = 0, Mon = 1, Tue = 2, Wed = 3, Thur = 4, Fri = 5, Sat = 6, Every Day = -1
+# DOW - Sun = 0, Mon = 1, Tue = 2, Wed = 3, Thur = 4, Fri = 5, Sat = 6, Every Day = -1
 # ENABLED - 0 entry is disabled, 1 enabled
+# MONTH - Month for a dated event (1-12)
+# DAY - Day of the month for a dated event (1-31)
+# YEAR - Year for a dated event (2016)
 # STARTTIME/ENDTIME - Start/End = 0 implies broadcast all day, HH:MM format
 # TIMEBEFORE - Number of minutes to start advertising object before STARTTIME
-# OBJNAME - Name of APRS Object, Max 9 characters
+# OBJNAME - Name of APRS Object, ax 9 characters
 # MHZ - Frequency associated with this object
 # LAT/LON - Latitude/Longitude of object
 # FREQ - How often to beacon object
@@ -41,21 +47,23 @@ sub get_defaults {
 
 sub get_objects {
     my @objects = (
-        # DAY ENABLED STARTTIME ENDTIME TIMEBEFORE OBJNAME MHZ LAT LON FREQ OFFSET TONE  HEIGHT POWER SYMBOL COMMENT
-        '0|1|21:00|22:00|15|NETATVCOM|146.760|38^02.380N|84^24.170W|5:00|-0.600||750|50|/N|9pm R30m AVT and Specialized Communications Net Sun',
-        '1|1|19:00|20:00|15|NET-ARES|146.865|37^43.778N|84^19.012W|5:00|-0.600|192.8|200|50|/N|7pm Madison County ARES Net Mon',
-        '2|1|20:00|21:00|15|NET-SWAP|145.370|37^43.778N|84^19.012W|5:00|-0.600|192.8|200|50|/N|8pm Amateur Swap Net Tue',
-        '3|1|20:30|21:30|15|NET-EMERG|146.715|37^43.778N|84^19.012W|5:00|-0.600|100.0|200|50|/N|8:30pm Wilderness Trail Emergency Net Wed',
-        '3|1|21:00|22:00|15|NET-ARES|146.760|38^02.380N|84^24.170W|5:00|-0.600||750|50|/N|9pm Fayette County ARES Net Wed',
-        '4|1|19:00|21:00|60|MEET-TECH|146.760|38^05.152N|84^29.347W|5:00|-0.600||750|50|/E|7pm BARS Technical Group Thur Basement Red Cross Building',
-        '4|1|19:30|20:30|15|NET-JAWS|145.490|37^53.077N|84^34.430W|5:00|-0.600||150|50|/N|7:30pm JAWS Rag Chew Net Thur',
-        '4|1|20:00|21:00|15|NET-ARES|145.330|38^02.973N|84^45.157W|5:00|-0.600||200|50|/N|8pm Woodford County ARES Net Thurs',
-        '4|1|20:45|21:45|15|NET-PRC|145.430|38^00.389N|84^13.606W|5:00|-0.600|203.5|200|50|/N|8:45pm Pioneer Amateur Radio Club Net Thur',
-        '4|1|21:00|22:00|15|NET-IRLP|146.940|38^02.856N|84^29.943W|5:00|-0.600|88.5|500|50|/N|9pm IRLP Wide Area Net Thur 9pm',
-        '6|1|11:00|13:00|60|BARSSHACK|146.760|38^05.152N|84^29.347W|5:00|-0.600||750|50|/E|11am BARS Shack Open Sat 11-1pm Red Cross Building',
-        '6|0|13:00|15:00|60|WORKSHOP|146.760|38^05.152N|84^29.347W|5:00|-0.600||750|50|/E|1pm BARS Radio Theory and Construction Workshop Sat 1-3pm Basement Red Cross Building',
-        '-1|1|0|0|15|IRLP-4945|146.940|38^02.856N|84^29.943W|10:00|-0.600|88.5|500|50|I0|R45m KY4K IRLP node 4945 in Lexington, KY',
-        '-1|1|0|0|15|146.940/R|146.940|38^02.856N|84^29.943W|10:00|-0.600|88.5|500|50|/r|R45m KY4K IRLP node 4945 in Lexington, KY',
+        # DOW ENABLED MONTH DAY YEAR STARTTIME ENDTIME TIMEBEFORE OBJNAME MHZ LAT LON FREQ OFFSET TONE  HEIGHT POWER SYMBOL COMMENT
+        '0|1||||21:00|22:00|15|NETATVCOM|146.760|38^02.380N|84^24.170W|5:00|-0.600||750|50|/N|9pm R30m AVT and Specialized Communications Net Sun',
+        '1|1||||19:00|20:00|15|NET-ARES|146.865|37^43.778N|84^19.012W|5:00|-0.600|192.8|200|50|/N|7pm Madison County ARES Net Mon',
+        '2|1||||20:00|21:00|15|NET-SWAP|145.370|37^43.778N|84^19.012W|5:00|-0.600|192.8|200|50|/N|8pm Amateur Swap Net Tue',
+        '3|1||||20:30|21:30|15|NET-EMERG|146.715|37^43.778N|84^19.012W|5:00|-0.600|100.0|200|50|/N|8:30pm Wilderness Trail Emergency Net Wed',
+        '3|1||||21:00|22:00|15|NET-ARES|146.760|38^02.380N|84^24.170W|5:00|-0.600||750|50|/N|9pm Fayette County ARES Net Wed',
+        '4|1||||19:00|21:00|60|MEET-TECH|146.760|38^05.152N|84^29.347W|5:00|-0.600||750|50|/E|7pm BARS Technical Group Thur Basement Red Cross Building',
+        '4|1||||19:30|20:30|15|NET-JAWS|145.490|37^53.077N|84^34.430W|5:00|-0.600||150|50|/N|7:30pm JAWS Rag Chew Net Thur',
+        '4|1||||20:00|21:00|15|NET-ARES|145.330|38^02.973N|84^45.157W|5:00|-0.600||200|50|/N|8pm Woodford County ARES Net Thurs',
+        '4|1||||20:45|21:45|15|NET-PRC|145.430|38^00.389N|84^13.606W|5:00|-0.600|203.5|200|50|/N|8:45pm Pioneer Amateur Radio Club Net Thur',
+        '4|1||||21:00|22:00|15|NET-IRLP|146.940|38^02.856N|84^29.943W|5:00|-0.600|88.5|500|50|/N|9pm IRLP Wide Area Net Thur 9pm',
+        '6|1||||11:00|13:00|60|BARSSHACK|146.760|38^05.152N|84^29.347W|5:00|-0.600||750|50|/E|11am BARS Shack Open Sat 11-1pm Red Cross Building',
+        '6|1||||13:00|15:00|60|WORKSHOP|146.760|38^05.152N|84^29.347W|5:00|-0.600||750|50|/E|1pm BARS Radio Theory and Construction Workshop Sat 1-3pm Basement Red Cross Building',
+        '-1|1|8|14|2016|8:00|14:00|60|BARSHAMFS|146.760|38^01.382N|84^54.372W|5:00|-0.600||750|50|/N|8am to 2pm Today BARS Hamfest',
+        '-1|1|9|24|2016|8:00|14:00|60|CK-HAMFST|146.865|37^43.778N|84^19.012W|5:00|-0.600|192.8|200|50|/N|8am to 2pm Today Central Kentucky Hamfest',
+        '-1|1||||0|0|15|IRLP-4945|146.940|38^02.856N|84^29.943W|10:00|-0.600|88.5|500|50|I0|R45m KY4K IRLP node 4945 in Lexington, KY',
+        '-1|1||||0|0|15|146.940/R|146.940|38^02.856N|84^29.943W|10:00|-0.600|88.5|500|50|/r|R45m KY4K IRLP node 4945 in Lexington, KY',
     );
     return (@objects);
 }
