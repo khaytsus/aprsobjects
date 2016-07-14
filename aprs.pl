36294b9ecd2d2554506030b73cb35df0787dfe7a
Walter Francis <wally@theblackmoor.net>
2016-07-14 11:05:43 -0400

    Add support for using iCal as a data source

--- aprs.pl
+++ aprs.pl
@@ -2,8 +2,9 @@
 
 use strict;
 use warnings;
+use Data::Dumper;
 
-our $VERSION = '1.1';
+our $VERSION = '1.2';
 
 # APRS Schedule generator for Direwolf
 # Based on http://www.aprs.org/info/netsked.txt
@@ -13,22 +14,12 @@ our $VERSION = '1.1';
 # that template and this output and use that as the final configuration.  Repeat this every N minutes,
 # checking to see if the configuration has changed and if so restart direwolf so it picks up the new info.
 
-# Todo
-## Send packets directly, not using direwolf
-### beacon -d "APZ101 WIDE2-1" -t 60 0 '!DDMM.mmN/DDDMM.mmW$Comment'
-### aprx maybe?
-### Or directly in Perl using KISS
-### http://search.cpan.org/~rbdavison/Device-TNC-0.03/lib/Device/TNC/KISS.pm
-### http://search.cpan.org/dist/Ham-APRS-FAP/IS.pm
-
-## Improve object date options
-### Perhaps read a public iCal
-
 # This perl program is generic, so we use our module for station-specific settings
 use aprsobjects;
 
 # Get default settings from our aprsobjects module
-my ( $moduleversion, $startinterval, $delayinterval, @outputs )
+my ( $debug, $moduleversion, $icalurl, $startinterval, $delayinterval,
+    @outputs )
     = get_defaults();
 
 # Test to make sure our versions match so we know our fields are going to be correct
@@ -39,11 +30,44 @@ if ( $VERSION ne $moduleversion ) {
         . $moduleversion . "\n";
 }
 
-# Get our objects from our aprsobjects module
-my (@objects) = get_objects();
-
 # Get our initial delay for advertisements which will be incremented for each additional object
 my $delay = $startinterval;
+
+# Create our empty objects variable to populate from our module or ical
+my @objects;
+
+# See if we're using iCal and if so, make sure iCal::Parser and LWP::Simple are available
+my $ical;
+
+if ($icalurl) {
+    $ical = eval {
+        require iCal::Parser;
+        iCal::Parser->import;
+
+        require LWP::Simple;
+        LWP::Simple->import(qw($ua get));
+        1;
+    };
+}
+
+# Get the current time and set up variables to use later
+my ($lt_sec,  $lt_min,  $lt_hour, $lt_mday, $lt_mon,
+    $lt_year, $lt_wday, $lt_yday, $lt_isdst
+) = localtime();
+$lt_year += 1900;
+$lt_mon  += 1;
+
+if ($ical) {
+
+    # Get our objects from our iCal URL
+    print "\n### Objects generated from iCal data\n";
+    (@objects) = ical_parse($icalurl);
+}
+else {
+    # Get our objects from our aprsobjects module
+    print "\n### Objects generated using module data\n";
+    (@objects) = get_objects();
+}
 
 # Iterate through each entry in the @objects array
 foreach my $entry (@objects) {
@@ -52,6 +76,8 @@ foreach my $entry (@objects) {
         $LAT,       $LON,     $FREQ,       $OFFSET,  $TONE,
         $HEIGHT,    $POWER,   $SYMBOL,     $COMMENT
     ) = split( /\|/xsm, $entry );
+
+    if ($debug) { print "### Object: $entry\n"; }
 
 # Determine the time at which we start advertising the object before the official start time
     my $start;
@@ -69,17 +95,12 @@ foreach my $entry (@objects) {
         $start = $STARTTIME;
     }
 
+    my $time = sprintf( "%02d%02d", $lt_hour, $lt_min );
+
     # We compare start/end as simple numbers, so strip out the colon
     $start =~ s/://g;
     my $end = $ENDTIME;
     $end =~ s/://g;
-
-    # Get the current time and build a numeric string to compare
-    my ( $sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst )
-        = localtime();
-    my $time = sprintf( "%02d%02d", $hour, $min );
-    $year += 1900;
-    $mon  += 1;
 
 # Determine if this object matches the day, is an every-day object, or is dated with todays date
 # If the object is enabled make sure that dated events are only matched on their proper date regardless
@@ -87,14 +108,14 @@ foreach my $entry (@objects) {
     if ($ENABLED
 
         # This object matches a single-day event, not a dated event
-        && ((   ( $DOW eq $wday && $DAY !~ /\d+/ )
+        && ((   ( $DOW eq $lt_wday && $DAY !~ /\d+/ )
 
                 # This object matches a daily event, not a dated event
                 || ( $DOW eq -1 && $DAY !~ /\d+/ )
             )
 
 # If it's not a single or daily event, test to see if this object matches todays date
-            || ( $MONTH eq $mon && $DAY eq $mday && $YEAR eq $year )
+            || ( $MONTH eq $lt_mon && $DAY eq $lt_mday && $YEAR eq $lt_year )
         )
         )
     {
@@ -181,9 +202,154 @@ foreach my $entry (@objects) {
     }
 }
 
+# Parse our iCal URL and return the same array we use in the module
+sub ical_parse {
+    my ($url) = @_;
+
+    if ($debug) { print "### ical_parse\n"; }
+
+    # Set our LWP defaults, maybe
+    #$ua->timeout(15);
+
+    my $file = get($url);
+    if (!defined($file))
+    {
+        print "\n## iCal download failed (check URL?)\n";
+        return;
+    }
+    my @returnobj;
+
+# Restrict our iCal event parsing to today and tomorrow, as the iCal data is in UTC
+# We restrict the results because it takes forever to process lots of events and we only care about today anyway
+    my $start_time = sprintf( "%d%02d%02d", $lt_year, $lt_mon, $lt_mday );
+    my $end_time   = sprintf( "%d%02d%02d", $lt_year, $lt_mon, $lt_mday + 1 );
+    my %defaults = ( start => $start_time, end => $end_time );
+    my $parser   = iCal::Parser->new(%defaults);
+    my $hash     = $parser->parse_strings($file);
+
+   # Get the events for today (specifically; ignore the rest of the iCal data)
+    my $todayhash  = $hash->{events}->{$lt_year}->{$lt_mon}->{$lt_mday};
+    my @todayarray = $todayhash;
+
+    # Iterate through each calendar UID (event)
+    foreach my $uid (@todayarray) {
+        my @uidkeys = keys %$uid;
+
+        # Iterate through each calendar object in this UID
+        foreach my $object (@uidkeys) {
+            my $DTSTART     = $todayhash->{$object}->{DTSTART};
+            my $DTEND       = $todayhash->{$object}->{DTEND};
+            my $allday      = $todayhash->{$object}->{allday};
+            my $DESCRIPTION = $todayhash->{$object}->{DESCRIPTION};
+            my $TRANSP      = $todayhash->{$object}->{TRANSP};
+            # Make sure our hours and minutes are padded to two digits
+            my $start       = sprintf( "%02d:%02d",
+                $DTSTART->{local_c}->{hour},
+                $DTSTART->{local_c}->{minute} );
+            my $end = sprintf( "%02d:%02d",
+                $DTEND->{local_c}->{hour},
+                $DTEND->{local_c}->{minute} );
+            my $month = $DTSTART->{local_c}->{month};
+            my $day   = $DTSTART->{local_c}->{day};
+            my $year  = $DTSTART->{local_c}->{year};
+            my $dow   = $DTSTART->{local_c}->{day_of_week};
+
+            # If allday isn't defined, set it to 0
+            unless ( defined($allday) ) {
+                $allday = 0;
+            }
+
+            # Clean up values the way we expect them to be for all-day events
+            #if ( $start eq "0:0" ) {
+            if ($allday) {
+                $dow   = "-1";
+                $start = "0";
+                $end   = "0";
+                $day   = '';
+                $month = '';
+                $year  = '';
+            }
+
+# Create a string like we'd use in the aprsobjects module so we can parse it the same
+            my $object = build_object(
+                $start, $end,  $allday, $dow, $month,
+                $day,   $year, $TRANSP, $DESCRIPTION
+            );
+            push( @returnobj, $object );
+        }
+    }
+    return @returnobj;
+}
+
+# Build a string of this objects fields
+sub build_object {
+    my ($start, $end,  $allday, $dow, $month,
+        $day,   $year, $TRANSP, $DESCRIPTION
+    ) = @_;
+
+    if ($debug) { print "### build_object\n"; }
+
+    # Split up DESCRIPTION into the fields we expect
+    my $objectparts = split_description($DESCRIPTION);
+
+# Use the "TRANSP" attribute to determine if this object is active or not based on Available/Busy in GCal
+    my $active = 1;
+    if ( $TRANSP eq "TRANSPARENT" ) {
+        $active = 0;
+    }
+
+    my $object = join( "|",
+        $dow, $active, $month, $day, $year, $start, $end, $objectparts );
+    return $object;
+}
+
+# Split up the description line and return a joined string
+sub split_description {
+    my ($description) = @_;
+
+# For now, disable uninitialized warnings here so we don't get noise from optional fields
+    no warnings 'uninitialized';
+
+    if ($debug) { print "### split_description\n"; }
+
+    # We literally get the string \n for carriage returns here.  Yuck?
+    my (@description) = split( /\\n/s, $description );
+
+    my ($timebefore, $objname, $mhz,    $lat,   $lon,    $freq,
+        $offset,     $tone,    $height, $power, $symbol, $comment
+    );
+
+    foreach my $desc (@description) {
+        my ( $key, $value ) = split( /:/, $desc, 2 );
+        if ( $key eq "TIMEBEFORE" ) { $timebefore = $value; }
+        if ( $key eq "OBJNAME" )    { $objname    = $value; }
+        if ( $key eq "MHZ" )        { $mhz        = $value; }
+        if ( $key eq "LAT" )        { $lat        = $value; }
+        if ( $key eq "LON" )        { $lon        = $value; }
+        if ( $key eq "FREQ" )       { $freq       = $value; }
+        if ( $key eq "OFFSET" )     { $offset     = $value; }
+        if ( $key eq "TONE" )       { $tone       = $value; }
+        if ( $key eq "HEIGHT" )     { $height     = $value; }
+        if ( $key eq "POWER" )      { $power      = $value; }
+        if ( $key eq "SYMBOL" )     { $symbol     = $value; }
+
+# We sometimes get backslashes back from iCal, so we need to strip them back out of the comment
+        if ( $key eq "COMMENT" ) {
+            $comment = $value;
+            $comment =~ s/\\//g;
+        }
+    }
+
+    my $objectparts = join( "|",
+        $timebefore, $objname, $mhz,    $lat,   $lon,    $freq,
+        $offset,     $tone,    $height, $power, $symbol, $comment );
+    return $objectparts;
+}
+
 # Update time reference to reflect desired additional delay
 # Used for HH:MM and MM:SS but the calculations are the same.  Pass in negative numbers to get an earlier time.
 sub update_delay {
+    if ($debug) { print "### update_delay\n"; }
     my ( $delaytmp, $delayintervaltmp ) = @_;
     my ( $sec_min, $sec_sec ) = split( /:/xsm, $delaytmp );
     my $sec_seconds = $sec_min * 60 + $sec_sec + $delayintervaltmp;
